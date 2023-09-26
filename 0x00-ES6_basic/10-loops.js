a23f329bb52fc94a14272833d12dfec87347f6d7
Kinotijoan <117837618+Kinotijoan@users.noreply.github.com>
2023-09-26 23:36:45 +0300

    javascript

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const arrayCopy = [...array];
+    let i = 0;
+    for (const el of array) {
+      arrayCopy[i] = appendString + el;
+      i += 1;
+    }
+  
+    return arrayCopy;
+  }
