980042d5aadf50969ac5a335809105dcfe11473c
Kinotijoan <117837618+Kinotijoan@users.noreply.github.com>
2023-10-04 06:09:37 +0300

    arrays

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,8 @@ export default function cleanSet(set, startString) {
   if (startString === '') {
     return '';
   }
-
   return Array.from(set)
-    .filter((word) => word.startsWith(startString))
-    .map((word) => word.slice(startString.length))
+    .filter((item) => item && item.startsWith(startString))
+    .map((item) => item.slice(startString.length))
     .join('-');
 }
